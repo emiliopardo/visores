fceae54732a09c4ab53f6a84f897f293e6c44841
Emilio Pardo <e.pardo.perez@gmail.com>
2021-10-26 13:51:33 +0200

    creado UserCrudController

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -73,7 +73,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->email;
+        return (string) $this->username;
     }
 
     /**

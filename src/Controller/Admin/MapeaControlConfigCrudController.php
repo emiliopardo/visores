49ddfb7e5c7ef71c0f3a85439dbf0ea85759beec
emiliopardo <e.pardo.perez@gmail.com>
2021-10-27 13:52:51 +0200

    creacion entidad controlConfig

---
+++ src/Controller/Admin/MapeaControlConfigCrudController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\MapeaControlConfig;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CodeEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class MapeaControlConfigCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return MapeaControlConfig::class;
+    }
+
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // the labels used to refer to this entity in titles, buttons, etc.
+            ->setEntityLabelInSingular('Configuración Mapea Control')
+            ->setEntityLabelInPlural('Configuraciones Mapea Control')
+            // the Symfony Security permission needed to manage the entity
+            // (none by default, so you can manage all instances of the entity)
+            // ->setEntityPermission('ROLE_EDITOR')
+        ;
+    }  
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            //IdField::new('id'),
+            AssociationField::new('mapeaControl')->setHelp('Mapea Control asociado'),   
+            TextField::new('description')->setHelp('Descripción de la configuración'),
+            CodeEditorField::new('config')->setLanguage('js')->setHelp('Parámetros de configuración')
+        ];
+    }
+
+}
---
+++ src/Entity/MapeaControlConfig.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MapeaControlConfigRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MapeaControlConfigRepository::class)
+ */
+class MapeaControlConfig
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $config;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=MapeaControl::class, inversedBy="mapeaControlConfigs")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $mapeaControl;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getConfig(): ?string
+    {
+        return $this->config;
+    }
+
+    public function setConfig(string $config): self
+    {
+        $this->config = $config;
+
+        return $this;
+    }
+
+    public function getMapeaControl(): ?MapeaControl
+    {
+        return $this->mapeaControl;
+    }
+
+    public function setMapeaControl(?MapeaControl $mapeaControl): self
+    {
+        $this->mapeaControl = $mapeaControl;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this -> getDescription();
+    }
+}

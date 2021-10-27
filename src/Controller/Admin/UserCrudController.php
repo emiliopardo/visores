8c19b568ea2d8499bbdca8e3c4121d7a20103a17
emiliopardo <e.pardo.perez@gmail.com>
2021-10-27 08:34:03 +0200

    incluida logica hasherPassword crud user controller

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -9,8 +9,22 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
-class UserCrudController extends AbstractCrudController
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserCrudController extends AbstractCrudController implements EventSubscriberInterface
 {
+
+    /** @var UserPasswordHasherInterface */
+      private $passwordHasher;
+
+     public function __construct(UserPasswordHasherInterface $passwordHasher)
+     {
+         $this->passwordHasher = $passwordHasher;
+     }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -26,5 +40,23 @@ class UserCrudController extends AbstractCrudController
             ArrayField::new('roles'),
         ];
     }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityPersistedEvent::class => 'hashPassword',
+            BeforeEntityUpdatedEvent::class => 'hashPassword',
+        ];
+    }
+
+        /** @internal */
+        public function hashPassword($event)
+        {
+            $user = $event->getEntityInstance();
+            if ($user instanceof User && $user->getPassword()) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            }
+        }
+
 }
 

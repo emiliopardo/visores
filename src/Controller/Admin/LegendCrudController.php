238d609921e4ea6c4a8fe90debda6e86e4f63518
emiliopardo <e.pardo.perez@gmail.com>
2021-10-28 13:31:38 +0200

    se definen labels LegendCrud

--- src/Controller/Admin/LegendCrudController.php
+++ src/Controller/Admin/LegendCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Legend;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -17,6 +18,18 @@ class LegendCrudController extends AbstractCrudController
     {
         return Legend::class;
     }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // the labels used to refer to this entity in titles, buttons, etc.
+            ->setEntityLabelInSingular('Leyenda')
+            ->setEntityLabelInPlural('Leyendas')
+            // the Symfony Security permission needed to manage the entity
+            // (none by default, so you can manage all instances of the entity)
+            // ->setEntityPermission('ROLE_EDITOR')
+        ;
+    }  
 
 
     public function configureFields(string $pageName): iterable

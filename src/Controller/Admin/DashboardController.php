d9e1547eba05287db1165899a676857b7574c5dc
emiliopardo <e.pardo.perez@gmail.com>
2021-10-29 11:00:50 +0200

    actualizados iconos

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -44,27 +44,28 @@ class DashboardController extends AbstractDashboardController
         // yield MenuItem::linkToCrud('Mapea Plugin', 'fas fa-globe', MapeaPlugin::class);
         // yield MenuItem::linkToCrud('Usuarios', 'fas fa-globe', User::class);
         return [
-            MenuItem::subMenu('Mapea', 'fa fa-article')->setSubItems([
-            MenuItem::linkToCrud('Mapea Core', 'fas fa-globe', MapeaCore::class)
+            MenuItem::linkToDashboard('Home', 'fa fa-home'),
+            MenuItem::subMenu('Mapea', 'fa fa-globe')->setSubItems([
+            MenuItem::linkToCrud('Mapea Core', 'fas fa-cog', MapeaCore::class)
             ]),
-            MenuItem::subMenu('Controles', 'fa fa-article')->setSubItems([
-                MenuItem::linkToCrud('Mapea Control', 'fas fa-globe', MapeaControl::class),
-                MenuItem::linkToCrud('Mapea Control Config', 'fas fa-globe', MapeaControlConfig::class)
+            MenuItem::subMenu('Controles', 'fas fa-solar-panel')->setSubItems([
+                MenuItem::linkToCrud('Mapea Control', 'fas fa-tv', MapeaControl::class),
+                MenuItem::linkToCrud('Mapea Control Config', 'fas fa-cogs', MapeaControlConfig::class)
             ]),
-            MenuItem::subMenu('Plugins', 'fa fa-article')->setSubItems([
-                MenuItem::linkToCrud('Mapea Plugin', 'fas fa-globe', MapeaPlugin::class),
-                MenuItem::linkToCrud('Mapea Plugin Config', 'fas fa-globe', MapeaPluginConfig::class)
+            MenuItem::subMenu('Plugins', 'fas fa-object-group')->setSubItems([
+                MenuItem::linkToCrud('Mapea Plugin', 'fas fa-object-ungroup', MapeaPlugin::class),
+                MenuItem::linkToCrud('Mapea Plugin Config', 'fas fa-cogs', MapeaPluginConfig::class)
             ]),
-            MenuItem::subMenu('Layers', 'fa fa-article')->setSubItems([
-                MenuItem::linkToCrud('Layer WMS', 'fas fa-globe', MapeaLayerWMS::class),
-                MenuItem::linkToCrud('Layer Geojson', 'fas fa-globe', MapeaLayerGeojson::class),
+            MenuItem::subMenu('Layers', 'fas fa-layer-group')->setSubItems([
+                MenuItem::linkToCrud('Layer WMS', 'fas fa-th', MapeaLayerWMS::class),
+                MenuItem::linkToCrud('Layer Geojson', 'fas fa-vector-square', MapeaLayerGeojson::class),
                 //MenuItem::linkToCrud('Mapea Plugin Config', 'fas fa-globe', MapeaPluginConfig::class)
             ]),
-            MenuItem::subMenu('Imagenes', 'fa fa-article')->setSubItems([
-                MenuItem::linkToCrud('Leyendas', 'fas fa-paint', Legend::class),
-                MenuItem::linkToCrud('Iconos', 'fas fa-paint', Icon::class)
+            MenuItem::subMenu('Imagenes', 'fas fa-images')->setSubItems([
+                MenuItem::linkToCrud('Leyendas', 'fas fa-image', Legend::class),
+                MenuItem::linkToCrud('Iconos', 'fas fa-icons', Icon::class)
             ]),
-            MenuItem::subMenu('Usuarios', 'fa fa-article')->setSubItems([
+            MenuItem::subMenu('Usuarios', 'fas fa-id-card')->setSubItems([
                 MenuItem::linkToCrud('Usuarios', 'fas fa-user', User::class)
             ])
             ];
